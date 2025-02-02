52b82a5c403837bb0956ac13a4c17b0c68c2fcf2
Vikas Ramaneti <vikas@Vikass-MacBook-Pro.local>
2025-02-02 15:00:31 -0800

    Personal Portfolio initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
-import React, { Suspense } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import './fonts/font-awesome/css/font-awesome.css';
-import Home from './pages/home/main';
-import routes from './data/routes';
-import './App.css';
+import React from "react";
+import "./App.scss";
+import Main from "./containers/Main";
 
-
-const { PUBLIC_URL } = process.env;
-
-// Every route - we lazy load so that each page can be chunked
-// NOTE that some of these chunks are very small. We should optimize
-// which pages are lazy loaded in the future.
-
-const App = () => (
-  <BrowserRouter basename={PUBLIC_URL}>
-    <Suspense fallback={<Home/>}>
-      <Switch>
-        <Route exact path="/" component={<Home/>}/>
-        {routes.map(route => (
-          <Route path={route.path} component={route.component} />
-        ))}
-      </Switch>
-    </Suspense>
-  </BrowserRouter>
-);
+function App() {
+  return (
+    <div>
+      <Main />
+    </div>
+  );
+}
 
 export default App;
